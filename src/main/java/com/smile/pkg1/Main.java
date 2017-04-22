3325801d5cf4b0e58c2d553cee5f4769413f93c2
hasithagamage <ghasithalakmal@gmail.com>
2017-04-22 11:07:24 +0530

    this is my initial commit

---
+++ src/main/java/com/smile/pkg1/Main.java
@@ -0,0 +1,13 @@
+package com.smile.pkg1;
+
+/**
+ * This is the main class
+ *
+ * @author Hasitha Gamage
+ *         on 4/22/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
